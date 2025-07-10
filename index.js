898509c0a659c09bb45e14e6dd3c7a6644375989
rizul <youremail@example.com>
2025-07-10 23:43:31 +0530

    Updating index.js file

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 var app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
-var items = [];
 
+var items = [];
 var ex = "working";
 
 app.get("/", function(req,res){
-    res.render("list", {ejes : items})
+    items = []; // Clear the items array on every page load/refresh
+    res.render("list", {ejes : items});
 });
 
 app.post("/", function(req,res){
