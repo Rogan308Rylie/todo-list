5c3982730df5dd74845de59243c8d67417524179
rizul <youremail@example.com>
2025-07-10 23:10:20 +0530

    fixed task not being added

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +15,9 @@ app.post("/", function (req, res) {
     const item = req.body.ele1;
     if (item && item.trim() !== "") {
         items.push(item);
-        res.sendStatus(200);
+        res.sendStatus(200);  // important: just send 200 for AJAX
     } else {
-        res.sendStatus(400); 
+        res.sendStatus(400);
     }
 });
 
